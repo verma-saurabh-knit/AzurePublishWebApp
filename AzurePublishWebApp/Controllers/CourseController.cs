21e28b195ce3f32b04fb5bccf70d2035d26732dd
mohini <verma.mohini@tcs.com>
2021-11-01 12:53:44 +0530

    Add project files.

---
+++ AzurePublishWebApp/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using AzurePublishWebApp.Models;
+using AzurePublishWebApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzurePublishWebApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService _course_service;
+        private readonly IConfiguration _configuration;
+
+        public CourseController(CourseService _svc, IConfiguration configuration)
+        {
+            _course_service = _svc;
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            // Use the configuration class to get the connection string
+            IEnumerable<Course> _course_list = _course_service.GetCourses(_configuration.GetConnectionString("SQLConnection"));
+            return View(_course_list);
+        }
+    }
+}
